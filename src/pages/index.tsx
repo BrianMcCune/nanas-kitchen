1af604e6b44f91797d69791f9291ac41ebde5f1c
Brian <brian.mccune1@yahoo.com>
2025-06-26 01:46:32 -0500

    color scheme

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,20 @@
 import React from "react";
 import "../styles/index.css";
+import { Link } from 'gatsby';
 
 
 export default function IndexPage() {
   return (
     <main className="landing">
       <div className="landing-content">
+        {/* <h1>NEW BRAUNFEL'S FAVORITE</h1> */}
         <h1>New Braunfel's Favorite</h1>
+        {/* <h1>FRESH BREAKFAST TACOS</h1> */}
         <h1>Fresh Breakfast Tacos</h1>
       </div>
+      <Link to="/menu" className="landing-button">
+        View Menu
+      </Link>
     </main>
   )
 }
