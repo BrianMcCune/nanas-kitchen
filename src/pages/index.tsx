8d8919706116f908e4e96f5819a39b242ca0d14c
Brian <brian.mccune1@yahoo.com>
2025-06-30 23:22:55 -0500

    menu base styling and content

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function IndexPage() {
     <main className="landing">
       <div className="landing-content">
         {/* <h1>NEW BRAUNFEL'S FAVORITE</h1> */}
-        <h1>New Braunfel's Favorite</h1>
+        <h1>New Braunfels' Favorite</h1>
         {/* <h1>FRESH BREAKFAST TACOS</h1> */}
         <h1>Fresh Breakfast Tacos</h1>
       </div>
--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,11 +1,78 @@
 import React from 'react';
+import "../styles/menu.css";
 
 const Menu = () => {
     return ( 
         <>
-            <section>
-                <h1>Menu</h1>
-                <h2>Tacos</h2>
+            <section className="menu">
+                <div className="menu-header">
+                    <h1>Nana's Kitchen Menu</h1>
+                    <p><em>Each item is freshly made to order — please allow extra time during busy hours. Menu may vary depending on inventory or special occasions.</em></p>
+                </div>
+                    
+                <div>
+                    <h2>Breakfast Tacos</h2>
+                    <ul>
+                        <li>Chorizo & Egg</li>
+                        <li>Spam & Egg</li>
+                        <li>Papas Rancheras</li>
+                        <li>Chorizo & Bean</li>
+                        <li>Migas</li>
+                        <li>Spicy Migas</li>
+                        <li>Sausage & Egg</li>
+                        <li>Chicharron & Egg</li>
+                        <li>Chicharron & Bean</li>
+                        <li>Spicy Chicharron & Egg</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2>Weekend Specials</h2>
+                    <ul>
+                        <li>Nana’s Taco (Bacon, Egg, Bean, Cheese)</li>
+                        <li>Carne Guisada</li>
+                        <li>Picadillo</li>
+                        <li>Barbacoa</li>
+                        <li>Brisket Ranchero</li>
+                        <li>Bacon & Egg</li>
+                        <li>Bean & Cheese</li>
+                        <li>Potato & Egg</li>
+                        <li>Pork Tamales (½ dozen or dozen)</li>
+                    </ul> 
+                </div>
+
+                <div>
+                    <h2>Menudo</h2>
+                    <ul>
+                        <li>Small (16 oz)</li>
+                        <li>Large (32 oz)</li>
+                    </ul>
+                </div>
+
+                <div>
+                    <h2>Add-Ons</h2>
+                    <ul>
+                        <li>Cheese</li>
+                        <li>Beans</li>
+                        <li>Potato</li>
+                        <li>Bacon (2 strips)</li>
+                    </ul>
+                </div>
+
+                <div className='drinks'>
+                    <h2>Drinks</h2>
+                    <ul>
+                        <li>Big Red</li>
+                        <li>Dr. Pepper</li>
+                        <li>Sprite</li>
+                        <li>Coke</li>
+                        <li>Water</li>
+                        <li>Apple Juice</li>
+                        <li>Orange Juice</li>
+                        <li>Glass Bottled Coke</li>
+                    </ul>
+                </div>
+
             </section>
         </>
      );
