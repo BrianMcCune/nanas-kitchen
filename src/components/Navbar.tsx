98adbc644dbf9fd45ec404c66a8598fb1a0953ac
Brian <brian.mccune1@yahoo.com>
2025-07-01 00:32:56 -0500

    navbar changes

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -13,9 +13,10 @@ const Navbar = () => {
           </Link>
           <h1>Nana's Kitchen</h1>
         </li>
+        <li><Link to="/">Home</Link></li>
         <li><Link to="/menu">Menu</Link></li>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/contact">Contact</Link></li>
+        {/* <li><Link to="/contact">Contact</Link></li> */}
         <li><Link to="/find-us">Find Us</Link></li>
       </ul>
     </nav>
