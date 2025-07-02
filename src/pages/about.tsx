af935d3a7b53f8bb9f4a01b95718d20aafc7d06d
Brian <brian.mccune1@yahoo.com>
2025-07-02 12:43:32 -0500

    about text fix and font changes

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -10,11 +10,11 @@ const About = () => {
                 <img src={nanaImg} alt="owner" className="owner" />
             </div>
             <section className="about-content">
-                <h1>Freshness in every order, family in every meal</h1>
+                <h1><span className='about-header-one'>Freshness in every order,</span><span className='about-header-two'> family in every meal</span></h1>
                 <p>
                     Family owned since 2021, we’re proud to be born and raised right here in New Braunfels. 
                     We specialize in delicious breakfast tacos made fresh every morning, alongside seasonal 
-                    homemade tamales crafted with care and tradition. Our slow-smoked brisket is a fan favorite, 
+                    homemade tamales crafted with care and tradition. Our Nana's Taco is a fan favorite, 
                     slow-cooked to perfection and full of flavor. Whether you're grabbing a quick bite or 
                     planning a special event, we offer catering to bring the taste of our kitchen right to you. 
                     Welcome to our table — where every bite feels like home.
